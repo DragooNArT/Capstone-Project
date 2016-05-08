bd1118efe5e310246ff0cab881de92f1fd20cb8d
DragooNArT-PC <olddrax@gmail.com>
2016-05-09 00:39:19 +0300

    added list entry skeleton, select item listener

--- brewers_notepad/src/main/java/com/example/brewersnotepad/activities/MainActivity.java
+++ brewers_notepad/src/main/java/com/example/brewersnotepad/activities/MainActivity.java
@@ -1,30 +1,34 @@
-package com.example.brewersnotepad;
+package com.example.brewersnotepad.activities;
 
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
+import com.example.brewersnotepad.R;
+import com.example.brewersnotepad.activities.adapters.RecipeListAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView mSideBarList;
+    private RecyclerView mBeerListView;
+    private RecipeListAdapter mBeerListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -42,8 +46,25 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        // Getting reference to the ActionBarDrawerToggle
+
+        mBeerListView = (RecyclerView) findViewById(R.id.beer_recipe_list_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mBeerListView.setLayoutManager(layoutManager);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mBeerListView.setHasFixedSize(true);
+        // The ForecastAdapter will take data from a source and
+        // use it to populate the RecyclerView it's attached to.
+        mBeerListAdapter = new RecipeListAdapter(this,findViewById(R.id.recepie_name));
+//
+        mBeerListView.addOnItemTouchListener(mBeerListAdapter);
+//        // specify an adapter (see also next example)
+        mBeerListView.setAdapter(mBeerListAdapter);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,4 +72,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
+
 }
---
+++ brewers_notepad/src/main/java/com/example/brewersnotepad/activities/adapters/RecipeListAdapter.java
@@ -0,0 +1,92 @@
+package com.example.brewersnotepad.activities.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.brewersnotepad.R;
+import com.example.brewersnotepad.activities.MainActivity;
+
+/**
+ * Created by DragooNArT-PC on 5/8/2016.
+ */
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder> implements  RecyclerView.OnItemTouchListener {
+    private View mEmptyView;
+    private MainActivity mActivity;
+
+
+    public RecipeListAdapter(MainActivity activity, View emptyView) {
+        this.mEmptyView = emptyView;
+        this.mActivity = activity;
+    }
+
+    @Override
+    public RecipeListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if ( parent instanceof RecyclerView ) {
+            int layoutId = -1;
+            switch (viewType) {
+                default: {
+                    layoutId = R.layout.beer_list_item;
+                    break;
+                }
+            }
+            View view = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+            view.setFocusable(true);
+            return new RecipeListViewHolder(view);
+        } else {
+            throw new RuntimeException("Not bound to RecyclerView");
+        }
+    }
+    String[] dummyData = new String[]{"Recepie 1","Recepie 2","Recepie 3"};
+
+    @Override
+    public void onBindViewHolder(RecipeListViewHolder holder, int position) {
+    String dummyDataEntry = dummyData[position];
+        holder.mRecipeName.setText(dummyDataEntry);
+    }
+
+    @Override
+    public int getItemCount() {
+        if ( null == dummyData ) return 0;
+        return dummyData.length;
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View childView = rv.findChildViewUnder(e.getX(),e.getY());
+        if(childView != null) {
+        //TODO find a way to fetch data entry from childView
+            //TODO transition to a new activity
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+    public static class RecipeListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView mRecipeName;
+
+        public RecipeListViewHolder(View view) {
+            super(view);
+            mRecipeName = (TextView) view.findViewById(R.id.recepie_name);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
